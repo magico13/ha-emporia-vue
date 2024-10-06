4081cf82587dd5209f9c6928066ba1ed5f90de74
Tim Laing <tim@laingcorp.co.uk>
2024-10-06 21:48:30 +0100

    Added OptionFlow and fixed formatting and typing issues

--- custom_components/emporia_vue/switch.py
+++ custom_components/emporia_vue/switch.py
@@ -1,10 +1,10 @@
 """Platform for switch integration."""
-import asyncio
-from datetime import timedelta
-import logging
-from typing import Any
 
-from requests import exceptions
+import asyncio
+import logging
+from datetime import timedelta
+from functools import cached_property
+from typing import Any
 
 from homeassistant.components.switch import SwitchDeviceClass, SwitchEntity
 from homeassistant.config_entries import ConfigEntry
@@ -16,14 +16,41 @@ from homeassistant.helpers.update_coordinator import (
     DataUpdateCoordinator,
     UpdateFailed,
 )
+from pyemvue import PyEmVue
 from pyemvue.device import ChargerDevice, OutletDevice, VueDevice
+from requests import exceptions
 
 from .charger_entity import EmporiaChargerEntity
 from .const import DOMAIN, VUE_DATA
 
-_LOGGER = logging.getLogger(__name__)
+_LOGGER: logging.Logger = logging.getLogger(__name__)
 
 device_information: dict[int, VueDevice] = {}  # data is the populated device objects
+
+
+async def __async_update_data(vue: PyEmVue):
+    """Fetch data from API endpoint.
+
+    This is the place to pre-process the data to lookup tables
+    so entities can quickly look up their data.
+    """
+    try:
+        # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+        # handled by the data update coordinator.
+        data = {}
+        loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
+        outlets: list[OutletDevice]
+        chargers: list[ChargerDevice]
+        (outlets, chargers) = await loop.run_in_executor(None, vue.get_devices_status)
+        if outlets:
+            for outlet in outlets:
+                data[outlet.device_gid] = outlet
+        if chargers:
+            for charger in chargers:
+                data[charger.device_gid] = charger
+        return data
+    except Exception as err:
+        raise UpdateFailed(f"Error communicating with Emporia API: {err}") from err
 
 
 async def async_setup_entry(
@@ -32,9 +59,9 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the sensor platform."""
-    vue = hass.data[DOMAIN][config_entry.entry_id][VUE_DATA]
+    vue: PyEmVue = hass.data[DOMAIN][config_entry.entry_id][VUE_DATA]
 
-    loop = asyncio.get_event_loop()
+    loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
     devices: list[VueDevice] = await loop.run_in_executor(None, vue.get_devices)
     for device in devices:
         if device.outlet or device.ev_charger:
@@ -42,30 +69,9 @@ async def async_setup_entry(
             device_information[device.device_gid] = device
 
     async def async_update_data():
-        """Fetch data from API endpoint.
-
-        This is the place to pre-process the data to lookup tables
-        so entities can quickly look up their data.
-        """
-        try:
-            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
-            # handled by the data update coordinator.
-            data = {}
-            loop = asyncio.get_event_loop()
-            outlets: list[OutletDevice]
-            chargers: list[ChargerDevice]
-            (outlets, chargers) = await loop.run_in_executor(
-                None, vue.get_devices_status
-            )
-            if outlets:
-                for outlet in outlets:
-                    data[outlet.device_gid] = outlet
-            if chargers:
-                for charger in chargers:
-                    data[charger.device_gid] = charger
-            return data
-        except Exception as err:
-            raise UpdateFailed(f"Error communicating with Emporia API: {err}") from err
+        return await __async_update_data(
+            vue=vue,
+        )
 
     coordinator = DataUpdateCoordinator(
         hass,
@@ -85,7 +91,7 @@ async def async_setup_entry(
             continue
         if device_information[gid].outlet:
             switches.append(EmporiaOutletSwitch(coordinator, vue, gid))
-        elif  device_information[gid].ev_charger:
+        elif device_information[gid].ev_charger:
             switches.append(
                 EmporiaChargerSwitch(
                     coordinator,
@@ -99,23 +105,22 @@ async def async_setup_entry(
     async_add_entities(switches)
 
 
-class EmporiaOutletSwitch(CoordinatorEntity, SwitchEntity):
+class EmporiaOutletSwitch(CoordinatorEntity, SwitchEntity):  # type: ignore
     """Representation of an Emporia Smart Outlet state."""
 
     def __init__(self, coordinator, vue, gid) -> None:
         """Pass coordinator to CoordinatorEntity."""
         super().__init__(coordinator)
-        # self._state = coordinator.data[index]['usage']
         self._vue = vue
         self._device_gid = gid
-        self._device = device_information[gid]
+        self._device: VueDevice = device_information[gid]
         self._attr_has_entity_name = True
         self._attr_name = None
         self._attr_device_class = SwitchDeviceClass.OUTLET
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the switch on."""
-        loop = asyncio.get_event_loop()
+        loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
         await loop.run_in_executor(
             None, self._vue.update_outlet, self.coordinator.data[self._device_gid], True
         )
@@ -123,7 +128,7 @@ class EmporiaOutletSwitch(CoordinatorEntity, SwitchEntity):
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the switch off."""
-        loop = asyncio.get_event_loop()
+        loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
         await loop.run_in_executor(
             None,
             self._vue.update_outlet,
@@ -132,7 +137,7 @@ class EmporiaOutletSwitch(CoordinatorEntity, SwitchEntity):
         )
         await self.coordinator.async_request_refresh()
 
-    @property
+    @cached_property
     def device_info(self) -> DeviceInfo:
         """Return the device information."""
         return DeviceInfo(
@@ -143,17 +148,24 @@ class EmporiaOutletSwitch(CoordinatorEntity, SwitchEntity):
             manufacturer="Emporia",
         )
 
-    @property
+    @cached_property
     def is_on(self) -> bool:
         """Return the state of the switch."""
         return self.coordinator.data[self._device_gid].outlet_on
 
-    @property
+    @cached_property
     def unique_id(self) -> str:
         """Unique ID for the switch."""
         return f"switch.emporia_vue.{self._device_gid}"
 
-class EmporiaChargerSwitch(EmporiaChargerEntity, SwitchEntity):
+    def turn_on(self, **kwargs: Any) -> None:
+        raise NotImplementedError()
+
+    def turn_off(self, **kwargs: Any) -> None:
+        raise NotImplementedError()
+
+
+class EmporiaChargerSwitch(EmporiaChargerEntity, SwitchEntity):  # type: ignore
     """Representation of an Emporia Charger switch state."""
 
     async def async_turn_on(self, **kwargs: Any) -> None:
@@ -164,14 +176,14 @@ class EmporiaChargerSwitch(EmporiaChargerEntity, SwitchEntity):
         """Turn the charger off."""
         await self._update_switch(False)
 
-    @property
+    @cached_property
     def is_on(self) -> bool:
         """Return the state of the switch."""
         return self.coordinator.data[self._device.device_gid].charger_on
 
-    async def _update_switch(self, on: bool):
+    async def _update_switch(self, on: bool) -> None:
         """Update the switch."""
-        loop = asyncio.get_event_loop()
+        loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
         try:
             await loop.run_in_executor(
                 None,
@@ -187,3 +199,9 @@ class EmporiaChargerSwitch(EmporiaChargerEntity, SwitchEntity):
             )
             raise
         await self._coordinator.async_request_refresh()
+
+    def turn_on(self, **kwargs: Any) -> None:
+        raise NotImplementedError()
+
+    def turn_off(self, **kwargs: Any) -> None:
+        raise NotImplementedError()
